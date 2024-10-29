736a6bc9f4d3663bad78def6df91c6655bd80133
Denis Julio <djms1234@gmail.com>
2024-10-28 21:34:13 -0300

    Primeiro commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/DenisJulio/marketplace-pit/components"
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	app := echo.New()
+	app.GET("/", HomeHandler)
+	app.Static("/static", "public/static")
+	app.Logger.Fatal(app.Start(":3000"))
+}
+
+func Render(ctx echo.Context, statusCode int, t templ.Component) error {
+	buf := templ.GetBuffer()
+	defer templ.ReleaseBuffer(buf)
+	if err := t.Render(ctx.Request().Context(), buf); err != nil {
+		return nil
+	}
+	return ctx.HTML(statusCode, buf.String())
+}
+
+func HomeHandler(c echo.Context) error {
+	return Render(c, http.StatusOK, components.Hello("Denis"))
+}
