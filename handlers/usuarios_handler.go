44292f237be6f4482576eb8013118e972e4f6047
Denis Julio <djms1234@gmail.com>
2024-11-14 07:18:31 -0300

    Refatora usuarios handler simplificando logica de funcao
    Adiciona ponteiro de cursor quando _hovering_ sobre a div que leva a
    minha conta

--- handlers/usuarios_handler.go
+++ handlers/usuarios_handler.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/DenisJulio/marketplace-pit/components"
-	"github.com/DenisJulio/marketplace-pit/model"
 	"github.com/DenisJulio/marketplace-pit/services"
 	"github.com/DenisJulio/marketplace-pit/utils"
 	"github.com/labstack/echo/v4"
@@ -75,21 +74,14 @@ func (h *UsuarioHandler) AutenticaUsuario(c echo.Context) error {
 
 func (h *UsuarioHandler) MostraBotaoDeEntrarNaConta(c echo.Context) error {
 	nomeDeUsuario, err := buscaNomeDeUsuarioDaSessao(c, h.logger)
-	var imagem string = ""
-	var usuario model.Usuario
-	var estaLogado bool
 	if err != nil || nomeDeUsuario == "" {
-		usuario = model.Usuario{}
-		estaLogado = false	
+		return render(c, http.StatusOK, components.EntrarNaConta(false, ""))
 	}
-	usuario, err = h.usuSvc.BuscaUsuarioPorNomeDeUsuario(nomeDeUsuario)
+	usuario, err := h.usuSvc.BuscaUsuarioPorNomeDeUsuario(nomeDeUsuario)
 	if err != nil {
-		estaLogado = false	
-	} else {
-		estaLogado = true	
-		imagem = *usuario.Imagem
+		return render(c, http.StatusOK, components.EntrarNaConta(false, ""))
 	}
-	return render(c, http.StatusOK, components.EntrarNaConta(estaLogado, imagem))
+	return render(c, http.StatusOK, components.EntrarNaConta(true, *usuario.Imagem))
 }
 
 func validaDadosParaLogin(nomeDeUsuario, senha string) error {
