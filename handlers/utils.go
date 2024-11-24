57359dc7243acd21bbbf4ac3c9fa477add981c9c
Denis Julio <djms1234@gmail.com>
2024-11-24 11:47:52 -0300

    Remove codigo inutilisado sobre gerencimento de sessao

--- handlers/utils.go
+++ handlers/utils.go
@@ -1,16 +1,9 @@
 package handlers
 
 import (
-	"net/http"
-	"time"
-
-	"github.com/DenisJulio/marketplace-pit/utils"
 	"github.com/a-h/templ"
-	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
-
-var sessaoStore = map[string]string{}
 
 func render(ctx echo.Context, statusCode int, t templ.Component) error {
 	buf := templ.GetBuffer()
@@ -19,35 +12,6 @@ func render(ctx echo.Context, statusCode int, t templ.Component) error {
 		return nil
 	}
 	return ctx.HTML(statusCode, buf.String())
-}
-
-func iniciarSessao(ctx echo.Context, nomeDeUsuario string) error {
-	sessaoID := uuid.NewString()
-	sessaoStore[sessaoID] = nomeDeUsuario
-	cookie := &http.Cookie{
-		Name:     "sessaoID",
-		Value:    sessaoID,
-		Path:     "/",
-		Expires:  time.Now().Add(24 * time.Hour),
-		HttpOnly: true,
-		Secure:   true,
-	}
-	ctx.SetCookie(cookie)
-	return nil
-}
-
-func buscaNomeDeUsuarioDaSessao(ctx echo.Context, logger utils.Logger) (string, error) {
-	logger.Debugf("Buscando sessao para: %s", ctx.Request().URL.Path)
-	cookie, err := ctx.Cookie("sessaoID")
-	if err != nil {
-		return "", err
-	}
-	un, ok := sessaoStore[cookie.Value]
-	if !ok {
-		return "", echo.ErrUnauthorized
-	}
-	logger.Debugf("Sessao encontrada para: usuario %s acessar %s", un, ctx.Request().URL.Path)
-	return un, nil
 }
 
 type tipoDeToast int
