acc41a79a980e5efb85154a7a6e4e23ff385381b
Denis Julio <djms1234@gmail.com>
2024-11-25 21:47:35 -0300

    Adiciona validacao de formulario para novo anuncio, à ser concluido.

--- handlers/anuncios_handler.go
+++ handlers/anuncios_handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"mime/multipart"
 	"net/http"
 	"strconv"
 
@@ -15,6 +16,7 @@ type AnunciosHandler struct {
 	anunSvc services.AnuncioServices
 	usuSvc  services.UsuarioService
 	oftSvc  services.OfertaService
+	imgSvc  services.ImagemService
 	logger  utils.Logger
 }
 
@@ -90,6 +92,57 @@ func (h *AnunciosHandler) CriaNovaOfertaParaAnuncio(c echo.Context) error {
 
 	// redirect to Home
 	return c.Redirect(http.StatusSeeOther, "/") // TODO: idealmete, redirecionar pelo cliente
+}
+
+func (h *AnunciosHandler) CriaNovoAnuncio(c echo.Context) error {
+	// Validate the form
+	nome, valor, descricao, imagem, err := h.validarFormularioDeAnuncio(c)
+
+	if err != nil {
+		h.logger.Errorf("Erro ao obter o arquivo de imagem: %v", err)
+		return c.String(http.StatusBadRequest, "Erro ao obter o arquivo de imagem")
+	}
+
+	// Save the image and get the public path
+	imagemPath, err := h.anunSvc.SalvaImagem(imagem)
+	if err != nil {
+		h.logger.Errorf("Erro ao salvar a imagem: %v", err)
+		return c.String(http.StatusInternalServerError, "Erro ao salvar a imagem")
+	}
+
+	// Create the new anuncio
+	anuncio := model.NewAnuncio(nome, valor, descricao, imagemPath)
+	if err := h.anunSvc.CriaNovoAnuncio(anuncio); err != nil {
+		h.logger.Errorf("Erro ao criar o novo anuncio: %v", err)
+		return c.String(http.StatusInternalServerError, "Erro ao criar o novo anuncio")
+	}
+
+	// Redirect to the anuncios page
+	return c.Redirect(http.StatusSeeOther, "/anuncios")
+}
+
+func (h *AnunciosHandler) validarFormularioDeAnuncio(c echo.Context) (string, float64, string, *multipart.FileHeader, error) {
+	nome := c.FormValue("nome")
+	valor, _ := strconv.ParseFloat(c.FormValue("valor"), 64)
+	descricao := c.FormValue("descricao")
+	imagem, err := c.FormFile("imagem")
+
+	if nome == "" {
+		return "", 0, "", nil, echo.NewHTTPError(http.StatusBadRequest, "O campo nome não pode estar vazio")
+	}
+	if valor == 0 {
+		return "", 0, "", nil, echo.NewHTTPError(http.StatusBadRequest, "O valor não pode ser zero")
+	}
+	if descricao == "" {
+		return "", 0, "", nil, echo.NewHTTPError(http.StatusBadRequest, "O campo descrição não pode estar vazio")
+	}
+	if err != nil {
+		return "", 0, "", nil, echo.NewHTTPError(http.StatusBadRequest, "O campo imagem não pode estar vazio")
+	}
+	if imagem.Size > 10*1024*1024 { // 10 MB
+		return "", 0, "", nil, echo.NewHTTPError(http.StatusBadRequest, "O tamanho da imagem não pode ser maior que 10 MB")
+	}
+	return nome, valor, descricao, imagem, nil
 }
 
 func (h *AnunciosHandler) extrairId(id string) (int, error) {
